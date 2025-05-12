4d9ab8e70e5999d6d2287902cbed8c48d2049672
Florence N <enolaworld@gmail.com>
2025-05-12 19:02:20 +0200

    ajout instruction dans le readme
    ajout nouveau fichier

---
+++ jour01/src/add.cpp
@@ -0,0 +1,13 @@
+
+// inclusion d'une bibliotheque d'en-tete
+#include <iostream>
+
+int main()
+/*std pour standard*/
+{
+    int a = 2, b = 5, c = 8, d = 15;
+
+    std::cout << a << std::endl;
+
+    return 0;
+}
