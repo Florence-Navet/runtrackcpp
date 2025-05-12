cf87d0927f779cf99f22425c2f78696b50ea6af7
Florence N <enolaworld@gmail.com>
2025-05-12 15:29:51 +0200

    feat: ajout nouvelles fonctionnalités sur utils

---
+++ jour01/src/utils.h
@@ -0,0 +1,11 @@
+
+/// @brief cette fonction additionne deux entiers
+/// @param a premiers valeurs a additionner
+/// @param b deuxieme valeur a additionner
+/// @return retourne la somme de a et b
+/*generate doxygem comment clic droit sur une fct pour ajouter une description*/
+
+int addition(int a, int b)
+{
+    return a + b;
+}
