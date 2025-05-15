bfa3bbd9d92aca05f557f99140a6637a52783b32
Florence N <enolaworld@gmail.com>
2025-05-15 12:25:22 +0200

    feat: job6 table de mutiplication

---
+++ jour01/src/Job6.cpp
@@ -0,0 +1,24 @@
+/**********************************************************************
+ * Écrire un programme en C++ qui affiche la table de multiplication  *
+ *  d’un entier donné par l’utilisateur.                              *           
+ **********************************************************************/
+
+#include <iostream>
+
+int main() {
+
+    int nombre;
+    
+
+        std::cout << "Entrez un nombre :";
+        std::cin >> nombre;
+
+        std::cout<< "\nTable de multipllication de "  << nombre << "\n";
+
+        for (int i = 1; i<= 10; i++) {
+            std::cout << nombre << " X " << i << " = " << nombre * i << std::endl;
+        }        
+        
+        return 0;
+
+}
