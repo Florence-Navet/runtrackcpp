acc8d2adc98abb0cdf2c8c03607d88ca0306ac4d
Florence N <enolaworld@gmail.com>
2025-05-17 10:33:25 +0200

    feat: jobt 15
    suite Fibonacci done

--- jour02/src/Job15.cpp
+++ jour02/src/Job15.cpp
@@ -2,3 +2,30 @@
 Écrire un programme en C++ permettant de générer la suite de Fibonacci
 jusqu’à une limite donnée par l’utilisateur.
  */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int limite;
+    cout << "Entrez la limite de la suite de Fibonacci : ";
+    cin >> limite;
+
+    int nombre1(0), nombre2(1), nombre3(0);
+
+    cout << "Suite de Fibonacci jusqu'à " << limite << " : " << nombre1 << ", " << nombre2;
+
+    while (nombre3 <= limite)
+    {
+        nombre3 = nombre1 + nombre2;
+        if (nombre3 > limite)
+            break;
+        cout << ", " << nombre3;
+        nombre1 = nombre2;
+        nombre2 = nombre3;
+    }
+    cout << endl;
+
+    return 0;
+}
