0f90725b97a75b9c913088c41d925c50db798ac7
Florence N <enolaworld@gmail.com>
2025-05-17 09:57:29 +0200

    feat: job10
    racine carree boucle while done

--- jour02/src/Job10.cpp
+++ jour02/src/Job10.cpp
@@ -4,3 +4,27 @@ négatives. Le programme s’arrêtera seulement si la valeur d'entrée est éga
 à 0. Exemple d'exécution :
 
 */
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double nombre(0);
+    cout << "Entrez un nombre : ";
+    cin >> nombre;
+    while (nombre != 0)
+    {
+        if (nombre < 0)
+        {
+            cout << "Erreur : la valeur ne peut pas etre negative." << endl;
+        }
+        else
+        {
+            cout << "La racine carree de " << nombre << " est : " << sqrt(nombre) << endl;
+        }
+        cout << "Entrez un nombre : ";
+        cin >> nombre;
+    }
+    return 0;
+}
