335125acae888b8d23ff267c360bff6408ceece3
Florence N <enolaworld@gmail.com>
2025-05-17 10:02:24 +0200

    feat : jobt 11
    version non recursive

--- jour02/src/Job11.cpp
+++ jour02/src/Job11.cpp
@@ -3,3 +3,23 @@
 nombre entier donné (n!). Par exemple : 0! = 1. Attention aux stack overflow !
 
 */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    unsigned int n;              // nombre entier >= 0
+    unsigned long long fact = 1; // type plus grand pour éviter débordement rapide
+
+    cout << "Entrez un nombre entier positif : ";
+    cin >> n;
+
+    for (unsigned int i = 1; i <= n; ++i)
+    {
+        fact *= i;
+    }
+
+    cout << n << "! = " << fact << endl;
+
+    return 0;
+}
